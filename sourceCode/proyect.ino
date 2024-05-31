fcefed57f810faad50655fed29cc2795e33d9dd8
Guejia21 <jonathanguejia@unicauca.edu.co>
2024-05-30 21:01:21 -0500

    The proyect is almost finish, Adrian didn´t  help us, now he has to do the entire report by himself, we are not going to help him

--- sourceCode/proyect.ino
+++ sourceCode/proyect.ino
@@ -26,26 +26,105 @@ byte colPins[COLS] = {30, 32, 34, 36}; //connect to the column pinouts of the ke
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 //Configuration for the leds
-#define  LED_RED 13
+#define  LED_RED 10
 #define  LED_GREEN 9
 #define  LED_BLUE 8 
 #define BUTTON_PIN 6
 
-// configuration for the alarm state melody 
-#define NOTE_G6 1568
-#define NOTE_E3 165
-#define NOTE_F3 175
-#define NOTE_G3 196
+
+//Sec Notes
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_DB3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_EB3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
 #define NOTE_GS3 208
-#define NOTE_A3 220
+#define NOTE_A3  220
 #define NOTE_AS3 233
-#define NOTE_B3 247
-#define NOTE_C4 262
-#define NOTE_C7 2093
-#define NOTE_E7 2637
-#define NOTE_G7 3136
-#define DHTTYPE DHT22 
-#define DHTPIN 7
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST     0
 
 int buzzer = 7; // El pin del buzzer activo
 int BloqueoMelody[] = {
@@ -92,11 +171,11 @@ const int pinHall = A1;
 int valueHall=0;
 
 //DHT
-#define DHTTYPE 11
+#define DHTTYPE 11 //Simultor 22
 #define DHTPIN 38
 DHT dht(DHTPIN,DHTTYPE);
-int h = 0;
-int t = 0;
+int hum = 0;
+int temp = 0;
 
 int tempHigh = 30;
 int tempLow = 15;
@@ -111,7 +190,6 @@ const char password[6] = {'1','2','3','4','5','#'};
 char buffer[6];
 int counter = -1;
 char tryCounter=0;
-
 // State Alias
 enum State
 {
@@ -132,7 +210,6 @@ enum Input
   btnPress = 3,
   hallExceeded = 4,
   tempLightExceeded = 5,
-
 	unknown = 6,
 };
 
@@ -177,10 +254,11 @@ void setupStateMachine()
   void leavingEventos(void);
   void leavingAlarma(void);
   void leavingMenu(void);
-	stateMachine.SetOnLeaving(Inicio, []() {Serial.println("Leaving Inicio"); });
+  void leavingInicio(void);
+	stateMachine.SetOnLeaving(Inicio, leavingInicio);
 	stateMachine.SetOnLeaving(ConfigMenu, leavingMenu );
 	stateMachine.SetOnLeaving(MonitorAmbiental, leavingAmbiental);
-	stateMachine.SetOnLeaving(Bloqueo, []() {Serial.println("Leaving Bloqueo"); });
+	stateMachine.SetOnLeaving(Bloqueo, leavingBloqueo);
   stateMachine.SetOnLeaving(Alarma, leavingAlarma);
   stateMachine.SetOnLeaving(MonitorEventos, leavingEventos);
 }
@@ -198,6 +276,10 @@ void setupStateMachine()
   void melodyExecutable(void);
   void Menu(void);
   void readButton(void);
+  void seguridad(void);
+  void failMelody(void);
+  void successMelody(void);
+  void sisBloqueado(void);
   AsyncTask taskReadLight(1000,true,readLight);
   AsyncTask taskReadTemp(1000,true,readTemp);
   AsyncTask taskReadHum(1000,true,readHum);
@@ -207,10 +289,15 @@ void setupStateMachine()
   AsyncTask taskPrintHallLcd(1000,true,printHallLcd);
   AsyncTask taskTempLightLimits(500,true,verifyTempLightLimits);
   AsyncTask taskHallLimits(1000,true,verifyHallLimit);
-  AsyncTask taskMenu(3000, true, Menu);
+  AsyncTask taskMenu(100, true, Menu);
   AsyncTask taskBlueLight(400,true,readBluelight);
   AsyncTask taskMelody(800,true,melodyExecutable);
   AsyncTask taskReadButton(100,true,readButton);
+  AsyncTask taskSecurity(1000,false,seguridad);
+  AsyncTask taskMelodyFail(800,false,failMelody);
+  AsyncTask taskMelodySuccess(800,false,successMelody);
+  AsyncTask taskBloqueo(500,true,sisBloqueado);
+
 
 // configuration for configMenu State
 const byte pwmPin = 6;
@@ -351,7 +438,7 @@ void reset_values() {
 
 void setup() 
 {
-	Serial.begin(9600);
+	Serial.begin(9600); //Simulator 115200
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   dht.begin();
@@ -400,7 +487,6 @@ void setup()
 
 void loop() 
 {
-  input = static_cast<Input>(readInput());
   taskSetTime.Update();
   taskReadButton.Update();
   //Updating tasks from Ambiental State
@@ -414,6 +500,11 @@ void loop()
   taskPrintHallLcd.Update();
   taskHallLimits.Update(); 
   taskMenu.Update(); 
+  //Updating security stuff
+  taskSecurity.Update();
+  taskMelodyFail.Update();
+  taskMelodySuccess.Update();
+  taskBloqueo.Update();
 // updating task for Alarma State
   taskBlueLight.Update();
   taskMelody.Update();
@@ -422,51 +513,43 @@ void loop()
   input = Input::unknown;
 }
 
-// Auxiliar function that reads the user input
-int readInput()
-{
-	Input currentInput = Input::unknown;
-	if (Serial.available())
-	{
-		char incomingChar = Serial.read();
-
-		switch (incomingChar)
-		{
-			case 'T': currentInput = Input::time; break;
-			case 'C': currentInput = Input::claveCorrecta; break;
-			case 'S': currentInput = Input::systemBlock; break;
-      case 'B': currentInput = Input::btnPress; break;
-			case 'H': currentInput = Input::hallExceeded; break;			
-      case 'L': currentInput = Input::tempLightExceeded; break;
-      default: break;
-		}
-	}
-
-	return currentInput;
-}
-
 // Auxiliar output functions that show the state debug
 void outputInicio()
 {
 	Serial.println("Inicio   Menu   Ambiental   Bloqueo   Alarma   Eventos");
 	Serial.println("  X                                                   ");
 	Serial.println();
-  //tryCounter = 0;
-  //counter = -1;
-  //seguridad();
+  tryCounter = 0;
+  counter = -1;
+  taskSecurity.Start();
 }
-
+void leavingInicio()
+{
+  taskSecurity.Stop();
+  taskMelodySuccess.Stop();
+}
 void outputMenu()
 {
 	Serial.println("Inicio   Menu   Ambiental   Bloqueo   Alarma   Eventos");
 	Serial.println("          X                                           ");
 	Serial.println();
+     menu.add_screen(screen1);
+    menu.add_screen(screen2);
+    menu.add_screen(screen3);
+    menu.add_screen(screen4);
+    menu.add_screen(screen5);
+    menu.add_screen(screen6);
+
+	strncpy(ledState_text, string_off, sizeof(string_off));
+
+	menu.update();
   taskMenu.Start();
   taskReadButton.Start();
 }
 void leavingMenu(){
   taskMenu.Stop();
   taskReadButton.Stop();
+  lcd.clear();
 }
 void outputMAmbiental()
 {
@@ -497,9 +580,16 @@ void outputBloqueo()
 	Serial.println("Inicio   Menu   Ambiental   Bloqueo   Alarma   Eventos");
 	Serial.println("                               X                      ");
 	Serial.println();
-  sisBloqueado();
   taskSetTime.SetIntervalMillis(10000);
   taskSetTime.Start();
+  taskBloqueo.Start();
+  taskMelodyFail.Start();
+}
+void leavingBloqueo(){
+  lcd.clear();
+  taskMelodyFail.Stop();
+  taskBloqueo.Stop();
+  digitalWrite(LED_RED,LOW);
 }
 void outputAlarma()
 {
@@ -510,12 +600,15 @@ void outputAlarma()
   taskBlueLight.Start();
   taskSetTime.SetIntervalMillis(4000);
   taskSetTime.Start();
+   taskReadButton.Start();
+   lcd.clear();
 }
 void leavingAlarma(){
   lcd.clear();
   taskMelody.Stop();
   taskBlueLight.Stop();
   digitalWrite(LED_BLUE, LOW);
+    taskSetTime.Stop();
 }
 
 void outputMEventos()
@@ -538,6 +631,7 @@ void leavingEventos(){
   taskSetTime.Stop();
 }
 void seguridad(){
+
   while(tryCounter < 3){
     if(counter==-1){
     lcd.clear();
@@ -555,6 +649,8 @@ void seguridad(){
       counter++;
       if(key == '#'){
         if(comparar(password,buffer,6)==true){
+          //claCorrecta();
+          successMelody();
           input = Input::claveCorrecta;
           return;
         }
@@ -563,7 +659,7 @@ void seguridad(){
           digitalWrite(LED_BLUE,HIGH);
           lcd.clear();
           lcd.print("Clave incorrecta");
-          delay(3000);
+          delay(1000);
           digitalWrite(LED_BLUE,LOW);
           counter=-1;
           lcd.clear();
@@ -586,14 +682,17 @@ void sisBloqueado(){
   lcd.print("SystemBlock");
   digitalWrite(LED_RED,HIGH);
   delay(5000);
-  lcd.clear();
   Input::time;
 }
 void claCorrecta(){
   lcd.clear();
+  taskMelodySuccess.Start();
   Serial.println("Clave correcta");
   lcd.print("Clave correcta");
   digitalWrite(LED_GREEN,HIGH);
+  delay(3000);
+  taskMelodySuccess.Stop();
+  digitalWrite(LED_GREEN,LOW);
 }
 
 void readLight(){
@@ -603,14 +702,14 @@ void readHall(){
   valueHall = analogRead(pinHall);
 }
 void readTemp(){
-  t = dht.readTemperature();
-  if(isnan(t)){
+  temp = dht.readTemperature();
+  if(isnan(temp)){
     Serial.println("Failed to read temperature");
   }
 }
 void readHum(){
-  h = dht.readHumidity();
-  if(isnan(h)){
+  hum = dht.readHumidity();
+  if(isnan(hum)){
     Serial.println("Failed to read temperature");
   }
 }
@@ -622,11 +721,11 @@ void printSensorsLcd(){
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("TEM:");
-  lcd.print(t);
+  lcd.print(temp);
   //Print humidity  
   lcd.setCursor(8,0);
   lcd.print("HUM:");
-  lcd.print(h);
+  lcd.print(hum);
   //Print light
   lcd.setCursor(0,1);
   lcd.print("LUZ:");
@@ -642,7 +741,7 @@ void printHallLcd(){
   lcd.print(valueHall);
 }
 void verifyTempLightLimits(){
-  if(t > tempHigh && h > humHigh){
+  if(temp > tempHigh && hum > humHigh){
     input = Input::tempLightExceeded;
   }
 }
@@ -653,7 +752,7 @@ void verifyHallLimit(){
   void readBluelight(){
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("BLUE:");
+    lcd.print("ALARMA ACTIVADA:");
     digitalWrite(LED_BLUE,HIGH);
   }
   void melodyExecutable(){
@@ -668,26 +767,7 @@ void verifyHallLimit(){
   }
   }
 
-void setupMenu() {
-   menu.add_screen(screen1);
-    menu.add_screen(screen2);
-    menu.add_screen(screen3);
-    menu.add_screen(screen4);
-    menu.add_screen(screen5);
-    menu.add_screen(screen6);
-
-	strncpy(ledState_text, string_off, sizeof(string_off));
-
-	menu.update();
-}
 void Menu (){
-  static bool initialized = false;
-  if (!initialized) {
-    setupMenu();
-    initialized = true;
-  }
-boolean aux=true;
-while (aux){
 char key = keypad.getKey();
 
   if (key) {
@@ -712,42 +792,53 @@ char key = keypad.getKey();
       Serial.println(F("ENTER button pressed"));
       menu.switch_focus();
     }
-    if(key == '#') {
-    aux=false;
-    input = Input::btnPress;
-    }
-
-    static unsigned long lastMillis = 0;
-    static unsigned int period = 3500;
-    if (millis() - lastMillis > period) {
-      lastMillis = millis();
-
-      Serial.print("LED turned ");
-      if (ledState == LOW) {
-        ledState = HIGH;
-        strncpy(ledState_text, string_on, sizeof(string_on));
-        Serial.println(ledState_text);
-        menu.update();
-      } else {
-        ledState = LOW;
-        strncpy(ledState_text, string_off, sizeof(string_off));
-        Serial.println(ledState_text);
-        menu.update();
-      }
-      digitalWrite(ledPin, ledState);
-
-      analogValue = analogRead(analogPin);
-      static unsigned short lastAnalogValue = 0;
-      if (analogValue != lastAnalogValue) {
-        lastAnalogValue = analogValue;
-        menu.update();
-      }
-    }
-  }}
+   }
 
 }
 void readButton(){
-  if(digitalRead(BUTTON_PIN)==LOW){
+  if(digitalRead(BUTTON_PIN)==HIGH){
     input = Input::btnPress;
+  }
+}
+
+void failMelody(){
+  int melody[] = {
+    NOTE_AS4, NOTE_AS4, NOTE_AS4,
+    NOTE_F5, NOTE_C6,
+    NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,
+
+  };
+
+  int durations[] = {
+    8, 8, 8,
+    2, 2,
+    8, 8, 8, 2, 4,
+  };
+  int size = sizeof(durations) / sizeof(int);
+
+  for (int note = 0; note < size; note++) {
+    //to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int duration = 1000 / durations[note];
+    tone(buzzer, melody[note], duration);
+
+    //to distinguish the notes, set a minimum time between them.
+    //the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = duration * 1.30;
+    delay(pauseBetweenNotes);
+
+    //stop the tone playing:
+    noTone(buzzer);
+  }
+}
+
+void successMelody(){
+  int  *melody=blockedMelody;
+  int  *durations=blockedDurations;
+  int length=blockedMelodyLength;
+  for (int i = 0; i < length; i++) {
+    tone(buzzer, melody[i], durations[i]);
+    delay(durations[i] * 1.20); // Delay entre notas
+    noTone(buzzer); // Asegurar que el buzzer esté apagado
   }
 }
